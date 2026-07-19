a21256414792c42745c43c31834157f91b005fc7
dev-pts <fw.engineer0@gmail.com>
2026-07-19 13:08:16 +0300

    fix name for port arrays

--- gen-tb-h.sh
+++ gen-tb-h.sh
@@ -6,7 +6,7 @@ filename=$2
 all_ports=$(${LOP_HDL} get-all-ports $topname $filename)
 
 for i in $all_ports; do
-	echo "SIG($i, $(echo $i | sed 's/\./___05F/g'))"
+	echo "SIG($i, $(echo $i | sed 's/\./___05F/g; s/\[/_/g; s/]//g'))"
 done > tb-sig.h
 
 cat > tb-top.h <<EOF
